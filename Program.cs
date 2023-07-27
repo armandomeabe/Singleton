a83ebe51e45c991196704d8a4be6e9159416d3a5
Armando Andrés Meabe <Armando.Meabe@brightstarr.com>
2023-07-27 16:21:54 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton
+{
+    public class MySingleton : Singleton<MySingleton>
+    {
+        private const string HelloWorldMessage = "Hello World - from MySingleton";
+
+        public string HelloWorld { get; private set; }
+
+        // Note: *** Private Constructor ***
+        private MySingleton()
+        {
+            // Set default message here.
+            HelloWorld = HelloWorldMessage;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mySingleton = MySingleton.Instance;
+            Console.WriteLine(mySingleton.HelloWorld);
+            Console.ReadKey();
+        }
+    }
+}
